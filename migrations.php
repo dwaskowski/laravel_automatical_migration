258124a5097e345f3bb6daa6bf13e98653e1922d
Dmitrij Waśkowski <dima@waskowscy.pl>
2013-11-19 14:29:22 +0100

    use class Artisan, add automatical Seeds
    Signed-off-by: Dmitrij Waśkowski <dima@waskowscy.pl>

--- migrations.php
+++ migrations.php
@@ -1,6 +1,7 @@
 <?php
 
 /**
+ * 
  * Automatical migration from modules
  *
  * @author Dmitrij Waśkowski <dima@waskowscy.pl>
@@ -22,24 +23,49 @@ $app->boot();
 /**
  * Function for automatical migrate
  */
-echo "Start automatical migration\n";
-echo "---------------------------\n";
-function search_migration_dir($path,&$app){
+function searchMigrationDir($path,&$app,&$path_artisan,&$seeds_class) {
     if($handle = opendir($path)){
         while (false !== ($entry = readdir($handle))) {
             $full_path = realpath($path."/".$entry);
             if($entry!='.' && $entry!='..' && is_dir($full_path)){
+                $this_path_artisan = $path_artisan."/".$entry;
                 if($entry=='migrations'){
-                    echo "run migrations from: ".$full_path."\n";
-                    $app['migrator']->run($full_path);
+                    echo "run Artisan migrate -path=".$path_artisan."/migrations\n";
+                    Artisan::call('migrate', array('--path' => $path_artisan.'/migrations'));                    
+                }elseif($entry=='seeds'){
+                    searchSeedsClass($full_path,$seeds_class);
                 }else{
-                    search_migration_dir($full_path,$app);
+                    searchMigrationDir($full_path,$app,$this_path_artisan,$seeds_class);
                 }                
             }
         }
     }
 }
-search_migration_dir($application_path,$app);
+function searchSeedsClass($path,&$seeds_class) {
+    if($handle = opendir($path)){
+        while (false !== ($entry = readdir($handle))) {
+            $full_path = realpath($path."/".$entry);
+            if($entry!='.' && $entry!='..' && !is_dir($full_path) && preg_match('/Seeder/', $entry)){
+                $seeds_class[] = preg_replace('/\.php/', '', $entry);
+            }
+        }        
+    }
+}
+function startSeeds(&$seeds_class){
+    foreach ($seeds_class as $seed) {
+        echo "run Artisan db:seed -=class=".$seed."\n";
+        Artisan::call('db:seed', array('--class' => $seed));
+    }
+}
+
+echo "Start automatical migration\n";
+echo "---------------------------\n";
+$path_artisan = 'app';
+$seeds_class = array();
+searchMigrationDir($application_path,$app,$path_artisan,$seeds_class);
+echo "Run Artisan modules:scan\n";
+Artisan::call('modules:scan');
+startSeeds($seeds_class);
 
 /**
  * Distroy Laravel application
